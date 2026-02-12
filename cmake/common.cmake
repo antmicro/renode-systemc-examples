6197a9bdac07f9857bebc0bc1537750e69b7ab5e
Filip Kokosinski <fkokosinski@antmicro.com>
2026-02-12 16:15:58 +0100

    [#88743] cmake: add preparations for native example building
    Signed-off-by: Filip Kokosinski <fkokosinski@antmicro.com>

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,15 +1,18 @@
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/../../cmake")
-
 file(GLOB_RECURSE systemc_sources systemc/src/*.cpp)
-add_executable(${EXAMPLE_NAME} ${systemc_sources})
+
+if(BUILD_SHARED)
+    add_library(${EXAMPLE_NAME} SHARED ${systemc_sources})
+    target_link_libraries(${EXAMPLE_NAME} PUBLIC renode_bridge_native)
+else()
+    add_executable(${EXAMPLE_NAME} ${systemc_sources})
+    target_link_libraries(${EXAMPLE_NAME} PRIVATE renode_bridge)
+endif()
 
 target_include_directories(${EXAMPLE_NAME} PUBLIC
     ${systemc_plugin}/include
     systemc/include
 )
 
-target_link_libraries(${EXAMPLE_NAME} PRIVATE renode_bridge)
-
-include(systemc)
-include(msvc)
-include(renode)
+include(../../cmake/systemc.cmake)
+include(../../cmake/msvc.cmake)
+include(../../cmake/renode.cmake)
